2cb308851b4f55133150aa968663c02dd4ae453b
NadzeyaShu <chunta89@gmail.com>
2022-08-04 13:31:16 +0400

    feat: implementation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let result = Math.abs(n)
+        .toString()
+        .split('')
+        .reverse()
+        .join('');
+    return Number(result);
 }
